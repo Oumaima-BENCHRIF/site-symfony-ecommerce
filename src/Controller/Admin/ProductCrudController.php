498f53869631113156dd23449f5b992b2d78723c
Oumaima BENCHRIF <ben.oumaimabenchrif@gmail.com>
2025-06-15 12:00:20 +0200

    add required

--- src/Controller/Admin/ProductCrudController.php
+++ src/Controller/Admin/ProductCrudController.php
@@ -32,11 +32,22 @@ class ProductCrudController extends AbstractCrudController
 
     public function configureFields(string $pageName): iterable
     {
+        $required=true;
+        if($pageName=='edit'){
+
+            $required=false;
+        }
         return [
             TextField::new('name')->setLabel("Name")->setHelp("Nom de votre produit"),
             SlugField ::new('slug')->setTargetFieldName("name")->setLabel("URL")->setHelp("URL de de votre produit généreé automatiquement"),
             TextEditorField::new('description')->setLabel("Description")->setHelp("Description de votre produit"),
-            ImageField::new('illustration')->setLabel("Image")->setHelp("Image du produit en 60x60 px")->setUploadedFileNamePattern('[year]-[month]-[day]-[contenthash].[extension]')->setBasePath('uploads/')->setUploadDir("/public/uploads") ,
+            ImageField::new('illustration')
+            ->setLabel("Image")
+            ->setHelp("Image du produit en 60x60 px")
+            ->setUploadedFileNamePattern('[year]-[month]-[day]-[contenthash].[extension]')
+            ->setBasePath('uploads/')
+            ->setUploadDir("/public/uploads") 
+            ->setRequired($required) ,
             NumberField::new('price')->setLabel("Prix H.T")->setHelp("Prix H.T du produit"),
             ChoiceField::new('tva')->setLabel('Taux de TVA')->setChoices([
                 '5,5%'=>'5.5',
