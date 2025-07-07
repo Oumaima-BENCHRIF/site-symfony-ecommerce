7e90307b8cddf58f02dcd0bb3bc7284f454bd9bd
Oumaima BENCHRIF <ben.oumaimabenchrif@gmail.com>
2025-07-07 02:32:28 +0200

    Créer un site ecommerce avec un backoffice, un système de paiement et de mail pour comprendre

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\HeaderRepository;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,12 +11,13 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(HeaderRepository $headerRepository,ProductRepository $productRepository): Response
     {
         // $name='Oumaima';
         // $table=['ab','nl'];
         return $this->render('home/index.html.twig',[
-        // 'name'=>$name,
+        'headers'=>$headerRepository->findAll(),
+        'productsInhomepage'=>$productRepository->findByIsHomepage(true),
         // 'table'=>$table,
         ]);
     }
